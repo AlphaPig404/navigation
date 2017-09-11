c1a555b2cd53a30df211e780ce98ac28e62bfebd
Nuluf <fulun2017@gmail.com>
2017-09-11 18:49:32 +0800

    change assetsPublicPath

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-default/index.css'
+import {Cascader,Input} from 'element-ui'
 
 import '@/assets/css/reset.css'
 import '@/assets/css/iconfont.css'
 
-Vue.use(ElementUI)
+Vue.use(Cascader)
+Vue.use(Input)
+
+// Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
